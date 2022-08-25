d9ba994fdf216f6676f2fa13764295adefad389a
Luca <luca@dashandslash.com>
2022-08-26 01:53:25 +0200

    Add APP_TEMPLATE folder with blackboard app project to be used as template

---
+++ APP_TEMPLATE/main.cpp
@@ -0,0 +1,44 @@
+#define SDL_MAIN_HANDLED
+
+#include <blackboard_app/app.h>
+#include <blackboard_app/gui.h>
+
+#include <imgui/imgui.h>
+#include <imgui/imgui_internal.h>
+#include <imguizmo/ImGuizmo.h>
+
+#include <filesystem>
+#include <iostream>
+#include <sstream>
+
+static const std::string state_name{"default_state"};
+
+void init()
+{
+    blackboard::app::gui::set_dracula_theme();
+}
+
+void app_update()
+{
+    blackboard::app::gui::dockspace();
+    ImGui::ShowDemoWindow();
+}
+
+int main(int argc, char *argv[])
+{
+    static const std::string headless_arg{"headless"};
+    if (argc > 1 && std::string(argv[1]) == headless_arg)
+    {
+        blackboard::app::App app(headless_arg.c_str(), blackboard::app::renderer::Api::NONE);
+        app.run();
+    }
+    else
+    {
+        blackboard::app::App app("Example SDL", blackboard::app::renderer::Api::AUTO);    // autodetect renderer api
+        app.on_update = app_update;
+        app.on_init = init;
+        app.run();
+    }
+
+    return 0;
+}
