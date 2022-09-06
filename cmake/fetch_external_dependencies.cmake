13ffe54d67b884d9d0b6c9f4964333217b98fa24
luca lolli <luca@dashandslash.com>
2022-09-06 23:29:03 +0200

    Bump SDL2 version

--- cmake/fetch_external_dependencies.cmake
+++ cmake/fetch_external_dependencies.cmake
@@ -8,7 +8,7 @@ set(FETCHCONTENT_BASE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/.external)
 # SDL
 FetchContent_Declare(sdl
     GIT_REPOSITORY https://github.com/libsdl-org/SDL.git
-    GIT_TAG release-2.0.20
+    GIT_TAG release-2.24.0
     GIT_SHALLOW 1
 )
 FetchContent_GetProperties(sdl)
