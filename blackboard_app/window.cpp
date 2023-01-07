f909061b990b51f341e779066dab4bdfb0e4e618
luca lolli <luca@dashandslash.com>
2023-01-07 12:09:58 +0100

    Remove defaul metal renderer option

--- blackboard_app/window.cpp
+++ blackboard_app/window.cpp
@@ -21,7 +21,7 @@ void Window::init_platform_window()
 {
   window = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height,
                             (fullscreen ? (SDL_WINDOW_FULLSCREEN_DESKTOP | SDL_WINDOW_BORDERLESS) : SDL_WINDOW_SHOWN) |
-                              SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_METAL);
+                              SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
 
 #ifdef _WIN32
   SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
